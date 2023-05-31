6ba28575d61a5aabfe2d6c47df357373e433c9a8
Lucas Rodriguez <110471982+lucasrdrz@users.noreply.github.com>
2023-05-31 20:24:01 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,request,jsonify
+
+
+app = Flask(__name__)
+#@app.route("/")
+#def home():
+#    return "Home"
+
+@app.route("/get-user/<user_id>")
+
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "name" : "John Doe",
+        "email" : "John.deo@example.com"
+    }
+
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+    
+    return jsonify(user_data), 200
+
+
+
+@app.route("/create-user", methods = ['POST'])
+def create_user():
+    data = request.get_json()
+
+    return jsonify(data), 201
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
